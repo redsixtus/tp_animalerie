d6ddb487403906cae5073f3cccaf920d04a39fd9
Your Name <you@example.com>
2021-06-08 21:18:16 +0200

    tp animalerie

---
+++ src/main/java/pet_shop/FishLivEnv.java
@@ -0,0 +1,17 @@
+package pet_shop;
+
+
+
+public enum FishLivEnv {
+    FRECH_WATER("eau_douce"), SEA_WATER("eau_mer")
+    ;
+    private String livingEnv;
+
+    FishLivEnv(String livingEnv) {
+        this.livingEnv = livingEnv;
+    }
+
+
+
+
+}
